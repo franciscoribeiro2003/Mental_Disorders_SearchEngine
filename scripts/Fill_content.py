0f909d525047e96b56a619d4fb2cedfa0ba82422
franciscoribeiro2003 <franciscorib2003@gmail.com>
2024-10-17 14:28:30 +0100

    infobox, number of views in the last 30days, number of edits, cleaned things like this [32] on the text

--- scripts/Fill_content.py
+++ scripts/Fill_content.py
@@ -4,6 +4,7 @@ import time
 from bs4 import BeautifulSoup
 import argparse
 import sys
+import re
 
 
 # Wikipedia base API URL
@@ -263,9 +264,7 @@ def edit_content(input_filename, output_filename):
 
     with open(output_filename, 'w', encoding='utf-8') as f:
         json.dump(disorders_data, f, ensure_ascii=False, indent=4)
-    #print("-----------------------------------------------")
-    #print("-----------------------------------------------")
-    #print(json.dumps(disorders_data, indent=4))
+
 
     print(f"Updated JSON file saved as: {output_filename}")
 
@@ -341,6 +340,208 @@ def selective_scrape_wikipedia_content(disorder_name):
             time.sleep(1)
     return ""
 
+##### Mode 5 #####
+def get_infobox(url):
+    failed_attempts = 0
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                bs = BeautifulSoup(response.text, 'html.parser')
+
+                table = bs.find('table', {'class' :'infobox'})
+
+                if not table:
+                    return None
+
+                result = {}
+
+                for row in table.find_all('tr'):
+                    header = row.find('th', {'class': 'infobox-label'})
+                    data = row.find('td', {'class': 'infobox-data'})
+                    if header and data:
+                        header_text = header.get_text(strip=True)
+                        print(f"HEADER: {header_text}")
+                        data_text = data.get_text(separator=' ', strip=True)
+                        print(f"        DATA: {data_text}")
+                        result[header_text] = data_text
+                return result
+            else:
+                print(f"Failed to fetch data for {url}, status code: {response.status_code}")
+                print("-----------------------------------------------")
+                failed_attempts += 1
+                if failed_attempts > 5:
+                    break
+                time.sleep(1)
+        except Exception as e:
+            print(f"Error fetching content for {url}: {e}")
+            failed_attempts += 1
+            if failed_attempts > 5:
+                break
+            time.sleep(1)
+    return ""
+
+def addInfobox(input_filename, output_filename):
+    # Load the existing JSON data
+    with open(input_filename, 'r', encoding='utf-8') as f:
+        disorders_data = json.load(f)
+
+    # Loop over each disorder and update the content
+    for disorder in disorders_data:
+        disorder_name = disorder["link"].split("/wiki/")[1]  # Extract the Wikipedia page title from the link
+        print(f"Fetching infobox for: {disorder['name']} ({disorder_name})")
+
+        # Scrape the infobox from Wikipedia
+        infobox = get_infobox(WIKI_PAGE_URL + disorder_name)
+        print(f"Infobox fetched for: {disorder['name']} ({disorder_name})")
+
+        # Update the JSON structure with the relevant infobox
+        disorder["infobox"] = infobox
+
+        # Optional: You can add a short delay between requests
+        time.sleep(0.05)  # Wait 0.1 second between requests to avoid overwhelming Wikipedia's servers
+
+    # Save the updated JSON data
+    with open(output_filename, 'w', encoding='utf-8') as f:
+        json.dump(disorders_data, f, ensure_ascii=False, indent=4)
+
+    print(f"Updated JSON file saved as: {output_filename}")
+
+##### Mode 6 #####
+
+
+def get_number_of_edits(url):
+    table = get_specific_table(url, 'Edit_history')
+    edits_row = table.find('tr', {'id': 'mw-pageinfo-edits'})
+    if not edits_row:
+        return 0
+    edits = edits_row.find_all('td')[1].get_text(strip=True)
+    return int(edits.replace(',', ''))
+
+
+def get_page_views(url):
+    failed_attempts = 0
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                table = soup.find('table', {'class': 'wikitable mw-page-info'})
+
+                if not table:
+                    raise Exception("Table with class 'wikitable mw-page-info' not found")
+
+                views_row = table.find('tr', {'id': 'mw-pvi-month-count'})
+                if not views_row:
+                    raise Exception("Row with id 'mw-pvi-month-count' not found")
+
+                views = views_row.find_all('td')[1].get_text(strip=True)
+                return int(views.replace(',', ''))
+            else:
+                print(f"Failed to fetch data for {url}, status code: {response.status_code}")
+                print("-----------------------------------------------")
+                failed_attempts += 1
+                if failed_attempts > 5:
+                    break
+                time.sleep(1)
+        except Exception as e:
+            print(f"Error fetching content for {url}: {e}")
+            failed_attempts += 1
+            if failed_attempts > 5:
+                break
+            time.sleep(1)
+    return 0
+
+
+def get_specific_table(url, header_id):
+    failed_attempts = 0
+    while True:
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                header = soup.find('h2', {'id': header_id})
+
+                if not header:
+                    raise Exception(f"Header with id '{header_id}' not found")
+
+                table = header.find_next('table', {'class': 'wikitable mw-page-info'})
+
+                if not table:
+                    raise Exception("Table with class 'wikitable mw-page-info' not found after the specified header")
+
+                return table
+            else:
+                print(f"Failed to fetch data for {url}, status code: {response.status_code}")
+                failed_attempts += 1
+                if failed_attempts > 5:
+                    break
+                time.sleep(1)
+        except Exception as e:
+            print(f"Error fetching content for {url}: {e}")
+            failed_attempts += 1
+            if failed_attempts > 5:
+                break
+            time.sleep(1)
+    return None
+
+
+def add_views_and_edits(input_filename, output_filename):
+    # Load the existing JSON data
+    with open(input_filename, 'r', encoding='utf-8') as f:
+        disorders_data = json.load(f)
+
+    # Loop over each disorder and update the content
+    for disorder in disorders_data:
+        disorder_name = disorder["link"].split("/wiki/")[1]  # Extract the Wikipedia page title from the link
+        print(f"Fetching views and edits for: {disorder['name']} ({disorder_name})")
+
+        # Scrape the number of edits from Wikipedia
+        edits = get_number_of_edits(WIKI_PAGE_URL + disorder_name + '?action=info#Basic_information')
+        print(f"Number of edits fetched for: {disorder['name']} ({disorder_name}): {edits}")
+
+        # Scrape the page views from Wikipedia
+        views = get_page_views(WIKI_PAGE_URL + disorder_name + '?action=info#Basic_information')
+        print(f"Page views fetched for: {disorder['name']} ({disorder_name}): {views}")
+
+        # Update the JSON structure with the relevant data
+        disorder["number_of_edits"] = edits
+        disorder["page_views"] = views
+
+        # Optional: You can add a short delay between requests
+        time.sleep(0.05)  # Wait 0.1 second between requests to avoid overwhelming Wikipedia's servers
+
+    # Save the updated JSON data
+    with open(output_filename, 'w', encoding='utf-8') as f:
+        json.dump(disorders_data, f, ensure_ascii=False, indent=4)
+
+    print(f"Updated JSON file saved as: {output_filename}")
+
+
+##### Mode 7 #####
+def clean_text(text):
+    # Use regex to remove patterns like [1], [ 408 ], [ 12 ], etc.
+    cleaned_text = re.sub(r'\[\s*\d+\s*\]', '', text)
+    return cleaned_text.strip()
+
+
+def clean_json_fields(input_filename, output_filename):
+    # Load the existing JSON data
+    with open(input_filename, 'r', encoding='utf-8') as f:
+        disorders_data = json.load(f)
+
+    # Loop over each disorder and clean the relevant fields
+    for disorder in disorders_data:
+        for field in ["content", "causes", "symptoms", "treatment", "diagnosis", "prevention", "epidemiology", "infobox"]:
+            if field in disorder and isinstance(disorder[field], str):
+                disorder[field] = clean_text(disorder[field])
+
+    # Save the cleaned JSON data
+    with open(output_filename, 'w', encoding='utf-8') as f:
+        json.dump(disorders_data, f, ensure_ascii=False, indent=4)
+
+    print(f"Cleaned JSON file saved as: {output_filename}")
+
 
 def main(mode):
     if mode == 1:
@@ -358,11 +559,23 @@ def main(mode):
     elif mode == 4:
         input_json_file = 'data/merged_disorders_FILLED_WITH_WIKIDATA_link.json'
         output_filename = 'data/merged_disorders_FILLED_WITH_PARTIAL_CONTENT.json'
-        #input_json_file = '/home/francisco/Desktop/MEIC_1YEAR/pri/Mental_Disorders_SearchEngine/data/debug.json'
-        #output_filename = '/home/francisco/Desktop/MEIC_1YEAR/pri/Mental_Disorders_SearchEngine/data/debug1.json'
         edit_content(input_json_file, output_filename)
+    elif mode == 5:
+        input_json_file = 'data/disorders_all_sections_fixed.json'
+        output_filename = 'data/disorders_plus_infobox.json'
+        addInfobox(input_json_file, output_filename)
+    elif mode == 6:
+        input_json_file = 'data/disorders_plus_infobox.json'
+        output_filename = 'data/disorders_views_plus_edits.json'
+        add_views_and_edits(input_json_file, output_filename)
+    elif mode == 7:
+        input_json_file = 'data/disorders_views_plus_edits.json'
+        output_filename = 'data/disorders_cleaned_final.json'
+        clean_json_fields(input_json_file, output_filename)
     else:
         print("Invalid mode selected. Use --help for more information.")
+
+
 
 
 if __name__ == "__main__":
@@ -370,11 +583,23 @@ if __name__ == "__main__":
         1: "Fill content and description of a JSON list of disorders.",
         2: "Associate new information to a JSON list of disorders.",
         3: "Associate Wikidata link to a JSON list of disorders and get the number of revisions.",
-        4: "Edit the content, scrape again the wikipedia, but this time select only a part of content"
+        4: "Edit the content, scrape again the wikipedia, but this time select only a part of content",
+        5: "Add the infobox (Scrape from Wikipedia) as a new field containing the infobox data in JSON format.",
+        6: "Add the wikipedia number of edits and page views over the last 30 days to the JSON",
+        7: "Remove redundant text from text fields, example [1], [2], [ 1 ], etc."
     }
-    
-    parser = argparse.ArgumentParser(description="Mental disorders gathering data.")
-    parser.add_argument('mode', type=int, choices=modes.keys(), nargs='?', help='Options: ' + ', \n'.join([f"{k} - {v}" for k, v in modes.items()]))
+
+    parser = argparse.ArgumentParser(
+        description="Mental disorders gathering data.",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument(
+        'mode',
+        type=int,
+        choices=modes.keys(),
+        nargs='?',
+        help='Options:\n' + '\n'.join([f"{k} - {v}" for k, v in modes.items()])
+    )
     
     args = parser.parse_args()
     
@@ -384,54 +609,3 @@ if __name__ == "__main__":
         sys.exit(1)
     
     main(args.mode)
-
-
-# # # Document
-
-# I want you to web scrap the wikipedia website to create and fill a Json file with the following structure:: Its important
-# to you to access the like of each entry and find the type of header of the thing I want you to find like for example: this one
-# <h2 id="Signs_and_symptoms">Signs and symptoms</h2> so you can know that the content is about the symptoms of the disease. and you extract the text
-# next to it. its important you to use some keywords so you can have more accuracy in the extraction of each item, for example in "Signs and symptoms" could be like 
-# "Signs and symptoms" or "Signs" or "Symptoms" or more, and the same thing for the other items.
-
-# For the code I provided, I want you to associate that task with the mode 2, you can create new funtions for it
-
-# Name:
-# # Type:
-# # Description:
-# # Link: 
-# # Content (bigger description):
-# # Causes: (optional/empty)
-# # Symptoms:(optional/empty)
-# # Treatment: (optional/empty)
-# # Diagnosis: (optional/empty)
-# # Prevention: (optional/empty)
-# # Epidemiology: (optional/empty)
-
-# # # Input
-# [
-#     {
-#         "name": "Agoraphobia",
-#         "type": "Anxiety disorders",
-#         "link": "https://en.wikipedia.org/wiki/Agoraphobia",
-#         "description": "\n\nAgoraphobia is a mental and behavioral disorder, specifically an anxiety disorder characterized by symptoms of anxiety in situations where the person perceives their environment to be unsafe with no easy way to escape. These situations can include public transit, shopping centers, crowds and queues, or simply being outside their home on their own. Being in these situations may result in a panic attack. Those affected will go to great lengths to avoid these situations. In severe cases, people may become completely unable to leave their homes.",
-#         "content": "\nAgoraphobia[1] is a mental and behavioral disorder,[5]  ......."
-#     ...
-# ]
-# # # Output
-# [
-#     {
-#         "name": "Agoraphobia",
-#         "type": "Anxiety disorders",
-#         "link": "https://en.wikipedia.org/wiki/Agoraphobia",
-#         "description": "\n\nAgoraphobia is a mental and behavioral disorder, specifically an anxiety disorder characterized by symptoms of anxiety in situations where the person perceives their environment to be unsafe with no easy way to escape. These situations can include public transit, shopping centers, crowds and queues, or simply being outside their home on their own. Being in these situations may result in a panic attack. Those affected will go to great lengths to avoid these situations. In severe cases, people may become completely unable to leave their homes.",
-#         "content": "\nAgoraphobia[1] is a mental and behavioral disorder,[5]  ......."
-#         "causes": "",
-#         "symptoms": "",
-#         "treatment": "",
-#         "diagnosis": "",
-#         "prevention": "",
-#         "epidemiology": "",
-#     }
-# ]
-
